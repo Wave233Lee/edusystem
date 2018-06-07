b909635fa87c8cedf11e1ae7ef6becf314168fe6
WaveLee <925863135@qq.com>
2018-06-07 18:58:32 +0800

    补完

---
+++ src/main/java/com/hnu/edusystem/EdusystemApplication.java
@@ -0,0 +1,12 @@
+package com.hnu.edusystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdusystemApplication.class, args);
+    }
+}
