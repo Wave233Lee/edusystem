9f61499ff6cec6226a33d6eae02f3e7707b559b5
Morph-3800 <dr826347380>
2018-06-08 01:21:37 +0800

    Student 1.0

---
+++ src/main/java/com/hnu/edusystem/controller/StudentController.java
@@ -0,0 +1,142 @@
+package com.hnu.edusystem.controller;
+
+import com.hnu.edusystem.domain.Student;
+import com.hnu.edusystem.domain.Result;
+import com.hnu.edusystem.service.StudentService;
+import com.hnu.edusystem.utils.ResultUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ * @Author: Morph
+ * @Date: 2018/6/8 0:44
+ */
+@RestController
+@RequestMapping(value = "/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    /**
+     * 新增
+     *
+     * @param student
+     * @param bindingResult
+     * @return
+     */
+    @RequestMapping(value = "/add")
+    public Result<Student> add(@Valid Student student ,BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResultUtil.error(bindingResult.getFieldError().getDefaultMessage().toString());
+        }
+
+        return ResultUtil.success(studentService.save(student));
+    }
+
+    /**
+     * 更新
+     *
+     * @param student
+     * @param bindingResult
+     * @return
+     */
+    @RequestMapping(value = "/update")
+    public Result<Student> update(@Valid Student student , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResultUtil.error(bindingResult.getFieldError().getDefaultMessage().toString());
+        }
+        return ResultUtil.success(studentService.update(student));
+    }
+
+    /**
+     * 通过id删除
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/deleteById")
+    public Result<Object> deleteById(String id) {
+        studentService.delete(id);
+        return ResultUtil.success();
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param students
+     * @return
+     */
+    @RequestMapping(value = "/deleteByIdBatch")
+    public Result<Object> deleteByIdbatch(@RequestBody Collection<Student> students) {
+        studentService.deleteInBatch(students);
+        return ResultUtil.success();
+    }
+
+    /**
+     * 通过id查找
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/getById")
+    public Result<Student> getById(String id) {
+        return ResultUtil.success(studentService.findOne(id));
+    }
+
+    /**
+     * 查找所有
+     *
+     * @return
+     */
+    @RequestMapping(value = "/getAll")
+    public Result<List<Student>> getAll() {
+        return ResultUtil.success(studentService.findAll());
+    }
+
+    /**
+     * 查询所有-分页
+     *
+     * @param page
+     * @param size
+     * @param sortfielName
+     * @param asc
+     * @return
+     */
+    @RequestMapping(value = "/getAllByPage")
+    public Result<Page<Student>> getAllPage(@RequestParam(value = "page" , defaultValue = "0" ) Integer page,
+                                            @RequestParam(value = "size" , defaultValue = "10") Integer size,
+                                            @RequestParam(value = "sortFielName" , defaultValue = "id") String sortfielName,
+                                            @RequestParam(value = "asc" , defaultValue = "1") Integer asc) {
+        return ResultUtil.success(studentService.findAllByPage(page , size , sortfielName ,asc));
+    }
+
+    /**
+     * 通过名称模糊查询-分页
+     *
+     * @param name
+     * @param page
+     * @param size
+     * @param sortFielName
+     * @param asc
+     * @return
+     */
+    @RequestMapping(value = "/getByNameLikeByPage")
+    public Result<Page<Student>> getByNameLikeByPage(@RequestParam(value = "name" , defaultValue = "") String name,
+                                                     @RequestParam(value = "page" , defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "size" , defaultValue = "10") Integer size,
+                                                     @RequestParam(value = "sortFielName" , defaultValue = "id") String sortFielName,
+                                                     @RequestParam(value = "asc" , defaultValue = "1") Integer asc) {
+        return ResultUtil.success(studentService.findBynameByPage(name , page, size , sortFielName , asc));
+    }
+}
+
---
+++ src/main/java/com/hnu/edusystem/service/StudentService.java
@@ -0,0 +1,154 @@
+package com.hnu.edusystem.service;
+
+import com.hnu.edusystem.domain.Student;
+import com.hnu.edusystem.exception.EduException;
+import com.hnu.edusystem.exception.EnumExceptions;
+import com.hnu.edusystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ * @Author: Morph
+ * @Date: 2018.6.8 0:39
+ */
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    /**
+     * 新增
+     *
+     * @param student
+     * @return
+     */
+    public Student save(Student student) {
+
+        //验证是否存在
+        if (student == null || (student.getId() != null && studentRepository.findOne(student.getId()) != null)) {
+            throw new EduException(EnumExceptions.ADD_FAILED_DUPLICATE);
+        }
+
+        return studentRepository.save(student);
+    }
+
+    /**
+     * 更新
+     *
+     * @param student
+     * @return
+     */
+    public Student update(Student student) {
+
+        //验证是否存在
+        if (student == null || student.getId() == null ||studentRepository.findOne(student.getId()) == null) {
+            throw new EduException(EnumExceptions.ADD_FAILED_DUPLICATE);
+        }
+
+        return studentRepository.save(student);
+    }
+
+    /**
+     *
+     * 删除
+     * @param id
+     */
+    public void delete(String id) {
+
+        //验证是否存在
+        if (studentRepository.findOne(id) == null) {
+            throw new EduException(EnumExceptions.DELETE_FAILED_NOT_EXIST);
+        }
+
+        studentRepository.delete(id);
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param students
+     */
+    public void deleteInBatch(Collection<Student> students) {
+        studentRepository.deleteInBatch(students);
+    }
+
+    /**
+     * 通过编码查询
+     *
+     * @param id
+     * @return
+     */
+    public Student findOne(String id) {
+        return studentRepository.findOne(id);
+    }
+
+    /**
+     * 查询全部
+     *
+     * @return
+     */
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    /**
+     * 查询所有-分页
+     *
+     * @param page
+     * @param size
+     * @param sortFieldName
+     * @param asc
+     * @return
+     */
+    public Page<Student> findAllByPage(Integer page , Integer size , String sortFieldName , Integer asc) {
+
+        //判断排序字段名是否存在
+        try {
+            Student.class.getDeclaredField(sortFieldName);
+        } catch (Exception e) {
+            //如果不存在就设置为id
+            sortFieldName = "id";
+        }
+
+        Sort sort = null;
+        if (asc == 0) {
+            sort = new Sort(Sort.Direction.DESC, sortFieldName);
+        } else {
+            sort = new Sort(Sort.Direction.ASC, sortFieldName);
+        }
+
+        Pageable pageable = new PageRequest(page, size, sort);
+        return studentRepository.findAll(pageable);
+    }
+
+
+    public Page<Student> findBynameByPage(String name , Integer page , Integer size , String sortFielName ,
+                                          Integer asc) {
+        // 判断排序字段名是否存在
+        try {
+            Student.class.getDeclaredField(sortFielName);
+        } catch (Exception e) {
+            // 如果不存在就设置为id
+            sortFielName = "id";
+        }
+
+        Sort sort = null;
+        if (asc == 0) {
+            sort = new Sort(Sort.Direction.DESC, sortFielName);
+        } else {
+            sort = new Sort(Sort.Direction.ASC, sortFielName);
+        }
+
+        Pageable pageable = new PageRequest(page, size, sort);
+        return studentRepository.findByNameLike("%" + name + "%", pageable);
+    }
+}
+
